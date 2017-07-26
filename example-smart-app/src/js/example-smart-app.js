e446ae43c984534ff4b9ed6b01471c6688d9974e
Akeem Spencer <aspencerpsu@gmail.com>
2017-07-26 14:10:52 -0400

    remove patient._id
    Remove the subject query parameter for pt. instance's id concatenation to string

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -25,7 +25,7 @@
         var care = smart.patient.api.fetchAll({
                     type:'CarePlan',
                     query: {
-                      subject: "Patient/"+pt._id
+                      subject: "Patient/"
                     }
         });
 

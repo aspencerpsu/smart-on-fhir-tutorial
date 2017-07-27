b32b458e2e0dd1cbd99c6508cd4246b6918a12c0
Akeem Spencer <aspencerpsu@gmail.com>
2017-07-27 09:39:42 -0400

    define person
    define person

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -65,6 +65,7 @@
           var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
           var hdl = byCodes('2085-9');
           var ldl = byCodes('2089-1');
+          var person;
 
           var p = defaultPatient();
           p.birthdate = dobStr;

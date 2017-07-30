5919cd8b359822b29980ffc891a0411c440165fd
Akeem Leighton Foster Spencer <aspencerpsu@gmail.com>
2017-07-30 16:37:05 -0400

    table selector switched to '.care'

--- example-smart-app/src/js/setupplans.js
+++ example-smart-app/src/js/setupplans.js
@@ -3,13 +3,13 @@
   window.findCarePlans = function(plans){
 
     /* Initialize the care plan table */
-    x = plans.length > 0 ? $('table > tbody').append("<tr></tr>") : "closing"; //There's no plans break the closure
+    x = plans.length > 0 ? $('.care > tbody').append("<tr></tr>") : "closing"; //There's no plans break the closure
     if (x=="closing"){
       return null;
     }
     
     plans.forEach(function(plan,index){
-      var last_row = $('table > tbody > tr:last-child')
+      var last_row = $('.care > tbody > tr:last-child')
       if (plan.category[0] !== undefined && plan.category[0].coding !== undefined){
         plan.category[0].coding.forEach(function(code){
           code.code != '' ? last_row.append("<td class=\'category\'>"+code.code+"<button id=\'pressed\'>overwrite?</button>"+"</td>") : "no codes";
@@ -19,11 +19,13 @@
       last_row.append("<td class=\'status\'>"+plan.status+"<button id=\'pressed\'>overwrite?</button>"+"</td>");
       /* end */
       if (plan.author !== undefined && plan.author.display !== undefined){
+        console.log(plan.author);
         plan.author.forEach(function(pt){
           pt.display != '' ? last_row.append("<td class=\'author\'>"+pt.display+"</td>") : "no authors";
         });
       }
       if (plan.text !== undefined && plan.text.div != ''){
+        console.log(plan.text);
         last_row.append("<td class=\'details>"+plan.text.div+"<button id=\'pressed\'>overwrite?</button>"+"</td>");
       } else {
         console.warn('No details enlisted for this encounter plan for subject, please advise');
@@ -41,7 +43,7 @@
                console.error("Something wrong with the updated time stamp for this plan");
          };
       //Initialize adding rows
-      plans.length -1 != index ? $('table > tbody').append("<tr></tr>") : '' //move onto the next thing
+      plans.length -1 != index ? $('.care > tbody').append("<tr></tr>") : '' //move onto the next thing
      });
   };
 

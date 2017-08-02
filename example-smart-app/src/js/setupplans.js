633babbc7241bb12b40f3d6186e2c896a3ddd31f
Akeem Leighton Foster Spencer <aspencerpsu@gmail.com>
2017-08-01 20:26:38 -0400

    setupplans executable

--- example-smart-app/src/js/setupplans.js
+++ example-smart-app/src/js/setupplans.js
@@ -9,35 +9,44 @@
     }
     
     plans.forEach(function(plan,index){
-      var last_row = $('.care > tbody > tr:last-child')
+      var last_row = $('.care > tbody > tr:last-child');
+      last_row.attr('id', plan.id);
+      /*CATEGORY TITLE*/
       if (plan.category[0] !== undefined && plan.category[0].coding !== undefined){
         plan.category[0].coding.forEach(function(code){
           code.code != '' ? last_row.append("<td class=\'category\'>"+code.code+"<button class=\'pressed\' id=\'"+plan.id+"\'>overwrite?</button>"+"</td>") : "no codes";
         });
       }
+      /*end*/
+      /*STATUS*/
       //Status IS ALWAYS defined within the carePlan Resource; No need for conditional logics
       last_row.append("<td class=\'status\' id=\'"+plan.id+"\'>"+plan.status+"<button class=\'pressed\' id=\'"+plan.id+"\'>overwrite?</button>"+"</td>");
       /* end */
+      /*AUTHOR*/
       if (plan.author !== undefined){
         plan.author.forEach(function(pt){
           pt.display != '' ? last_row.append("<td class=\'author\'>"+pt.display+"</td>") : "no authors";
         });
       }
+      /* end */
+      /* DETAIL */
       if (plan.text !== undefined && plan.text.div != ''){
-        last_row.append("<td class=\'details\'>"+plan.text.div+"<button class=\'subjectdetails\'>variguard?</button>"+"</td>");
+        last_row.append("<td class=\'details\'>"+plan.text.div+"<button class=\'subjectdetails\' id=\'" + plan.id + "\'>variguard?</button>"+"</td>");
         $('.details > div').addClass('plan-raw'); //For Later
       } else {
         console.warn('No details enlisted for this encounter plan for subject, please advise');
       };
+      /* end */
+      /* MODIFICATIONS */
       if (plan.meta == undefined || plan.meta.lastUpdated == undefined){
         if (plan.modified == undefined){
           console.warn('No log sheet for patient\'s care provision, please address immediately');
         }
         else if (plan.modified !== undefined) {
-          last_row.append("<td>"+plan.modified+"</td>");
+          last_row.append("<td class=\'modified\' id=\'"+plan.id+"\'>"+plan.modified+"</td>");
         }
        } else if (plan.meta !== undefined && plan.meta.lastUpdated !== undefined){
-         last_row.append("<td>"+plan.meta.lastUpdated+"</td>"); } 
+          last_row.append("<td class=\'modified\' id=\'"+plan.id+"\'>"+plan.modified+"</td>");
              else{
                console.error("Something wrong with the updated time stamp for this plan");
          };

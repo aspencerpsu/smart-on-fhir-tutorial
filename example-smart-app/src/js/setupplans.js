10f2feef1f06b1ef77bef13e0c333e68afd4a12f
Akeem Leighton Foster Spencer <aspencerpsu@gmail.com>
2017-08-02 09:21:14 -0400

    changed the status column to selection elements

--- example-smart-app/src/js/setupplans.js
+++ example-smart-app/src/js/setupplans.js
@@ -2,6 +2,7 @@
 
   window.findCarePlans = function(plans){
 
+
     /* Initialize the care plan table */
     x = plans.length > 0 ? $('.care > tbody').append("<tr></tr>") : "closing"; //There's no plans break the closure
     if (x=="closing"){
@@ -18,9 +19,13 @@
         });
       }
       /*end*/
+
       /*STATUS*/
       //Status IS ALWAYS defined within the carePlan Resource; No need for conditional logics
-      last_row.append("<td class=\'status\' id=\'"+plan.id+"\'>"+plan.status+"<button class=\'pressed\' id=\'"+plan.id+"\'>overwrite?</button>"+"</td>");
+      last_row.append("<td class=\'status\'>"+"<select class=multiple id='"+plan.id"'></select>"+"</td>");
+      statuses = ['active', 'cancelled', 'suspended', 'completed', 'draft'];
+      rotatethroughstatus(plan.status, statuses, plan.id);
+
       /* end */
       /*AUTHOR*/
       if (plan.author !== undefined){
@@ -51,5 +56,11 @@
       plans.length -1 != index ? $('.care > tbody').append("<tr></tr>") : '' //move onto the next thing
      });
   };
+
+  function rotatethroughstatus(stat, arr, id){
+   arr.map(function(val, ind){ val == stat ? $('.multiple #'+id).prepend("<option selected=\'selected\'>"+stat+"</option>") : 
+                                             $('.multiple #'+id).append("<option>"+stat+"</option>");
+   });
+  }
 
 })(window);

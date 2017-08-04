48aea1fc131b1e186697d6a6a3beffa6e3baf030
Akeem Leighton Foster Spencer <aspencerpsu@gmail.com>
2017-08-04 16:26:01 -0400

    introduction to sentinel

---
+++ example-smart-app/src/js/sentinels.js
@@ -0,0 +1,40 @@
+(function(window){
+  function SentinelInstance() {
+    return {
+            row: '',
+            pres: [],
+            memos: 0,
+            isUpdated: false //default
+           }
+  }
+
+  $.when(plansReady).fail(function(){
+    console.debug("No care plans defined for subject");
+    delete newaddendums; //remove from heap??
+  });
+
+  $.when(plansReady).done(function(){
+    var sentinel;
+    var plansUpdated = $('.details > .plan-raw').map(function(index, value){
+      sentinel = new SentintelInstance();//create a new sentinal instance for the providers list of patients;
+      sentinel.memos = $(this).children('pre').length; //used as a justification for a change made to the list;
+      sentinel.pres  = $(this).children('pre');
+      sentinel.row = $(this).parent().parent().attr('class'); //the row corresponding to this element;
+      return sentinel;
+     }).get();
+  });
+  
+  window.newaddendums = function(){
+
+    return setInterval(plansUpdated.forEach(function(index,element){
+      pt = $('#fname').text() + " " + $('#lname').text();
+      $row = value.row;
+      if (element.memos != $('tr.'+$row+' .details > .plan-raw > pre').length){
+        var plans = $('tr.'+$row+' .details > .plan-raw > pre');
+        element.isUpdated = true
+        afas_or = confirm("Patient "+pt+"'s information has changed, do you want to initiate AFAS to send a message to the proxy?");
+        afas_or ? console.log("Prompt a web socket to send pubsub information") : [element.isUpdated = false, element.memos=plans.length, element.pres = plans];
+      }
+    }), 15000);
+  }
+})(window);

ce7549a19a35b8045d90a15d201e5c761be6eeb4
Akeem Leighton Foster Spencer <aspencerpsu@gmail.com>
2017-08-05 14:29:45 -0400

    parameter added to newAddendums

--- example-smart-app/src/js/sentinels.js
+++ example-smart-app/src/js/sentinels.js
@@ -11,7 +11,7 @@
            }
   };
 
-  function myAddies(){
+  function myAddies(plansUpdated){
     return plansUpdated.forEach(function(element, index){
       console.log("at least it's running on a regular interval");
       pt = $('#fname').text() + " " + $('#lname').text();
@@ -25,9 +25,9 @@
     })
   }     
 
-  window.newAddendums = function(){
+  window.newAddendums = function(plans){
 
-    return setInterval(function(){myAddies}, 15000);
+    return setInterval(myAddies(plans), 15000);
   };
 
   $.when(plansReady).fail(function(){
@@ -36,16 +36,15 @@
   });
 
   $.when(plansReady).done(function(){
-    console.debug("I am prepared!");
     var sentinel;
-    plansUpdated = $('.details > .plan-raw').map(function(index, value){
+    plansUpdated = $('.care > .details > .plan-raw').map(function(index, value){
       sentinel = new SentinelInstance();//create a new sentinal instance for the providers list of patients;
       sentinel.memos = $(this).children('pre').length; //used as a justification for a change made to the list;
       sentinel.pres  = $(this).children('pre');
       sentinel.row = $(this).parent().parent().attr('class'); //the row corresponding to this element;
       return sentinel;
      }).get();
-    var intervalID = window.newAddendums();
+    var intervalID = window.newAddendums(plansUpdated);
     console.log(intervalID);
   });
   

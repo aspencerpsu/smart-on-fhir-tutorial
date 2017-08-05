f37f17408505a60bd99350c563e5c8b2bc8cea35
Akeem Leighton Foster Spencer <aspencerpsu@gmail.com>
2017-08-05 14:47:06 -0400

    plansUpdated'

--- example-smart-app/src/js/sentinels.js
+++ example-smart-app/src/js/sentinels.js
@@ -22,7 +22,7 @@
         afas_or = confirm("Patient "+pt+"'s information has changed, do you want to initiate AFAS to send a message to the proxy?");
         afas_or ? console.log("Prompt a web socket to send pubsub information") : [element.isUpdated = false, element.memos=plans.length, element.pres = plans];
       }
-    })
+    });
   }     
 
   window.newAddendums = function(plans){
@@ -37,13 +37,14 @@
 
   $.when(plansReady).done(function(){
     var sentinel;
-    plansUpdated = $('.care > .details > .plan-raw').map(function(index, value){
+    var plansUpdated = $('.care > .details > .plan-raw').map(function(index, value){
       sentinel = new SentinelInstance();//create a new sentinal instance for the providers list of patients;
       sentinel.memos = $(this).children('pre').length; //used as a justification for a change made to the list;
       sentinel.pres  = $(this).children('pre');
       sentinel.row = $(this).parent().parent().attr('class'); //the row corresponding to this element;
       return sentinel;
      }).get();
+    console.log(plansUpdated);
     var intervalID = window.newAddendums(plansUpdated);
     console.log(intervalID);
   });

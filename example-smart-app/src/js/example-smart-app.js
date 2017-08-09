8b747806b4b244b3d602bfb8fbdbbb3825cd3e03
Akeem Leighton Foster Spencer <aspencerpsu@gmail.com>
2017-08-09 09:32:15 -0400

    at the end of the list, return the true value

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -191,8 +191,8 @@
        counter = 0;
        //names aren't congruous, go to next on kins list
      };
-      goon = index == kins.length -1 ? false : 'false';
-      if (goon == false){return false} else { };//continue through the next on the kin list
+      goon = index == kins.length -1 ? false : true;
+      if (goon == false){return false} else {return true };//continue through the next on the kin list
    });
 
  };

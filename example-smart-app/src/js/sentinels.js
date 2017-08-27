c282a86df01a1ca96d61594cdb3ee025cdaff8d2
Akeem Spencer <aspencerpsu@gmail.com>
2017-08-26 23:00:38 -0400

    else statement if the sentinel didn't parse and catch any patterns

--- example-smart-app/src/js/sentinels.js
+++ example-smart-app/src/js/sentinels.js
@@ -18,7 +18,6 @@
  
 
     function addToAFAS(){
-      console.debug("at least it's running on a regular interval");
       return plansUpdated.forEach(function(element, index){
         pt = $('#fname').text() + " " + $('#lname').text();
         $row = element.row;
@@ -31,64 +30,70 @@
             afas_or = confirm("Patient "+pt+"'s information has changed, do you want to initiate AFAS to send a message to the proxy?");
             if (afas_or) {
               first_message = plans.filter(':first').text();
+	     
               /*TODO: add the user information when app is registered with user */
               /*XXX: Default is myself */
               provider = {name: "portal", organization: {number: "+18453712125"}}
               parseMessage = parseForStatus(first_message,pt, provider);
-              var contacts = $('.contacts > tbody > .names > td'); //list of the NAMES of hcproxies
-              contacts = contacts.map(function(){ return $(this).text();}).get();
-	      //Initialize the contacts array
-              parseMessage.contacts = [];
-	      //
-	      //add the name of the patient to the cluster
-	      parseMessage.patient = pt;
+	      if (parseMessage == undefined){
+	      	alert("You're message doesn't qualify for AFAS standards, please utilize afas manually or catch up on the README:"+
+			"\n https://afasemergencynetworks.com/faqs/");
+		return;
+	      } else {
+		  var contacts = $('.contacts > tbody > .names > td'); //list of the NAMES of hcproxies
+		  contacts = contacts.map(function(){ return $(this).text();}).get();
+		  //Initialize the contacts array
+		  parseMessage.contacts = [];
+	      	  //
+		  //add the name of the patient to the cluster
+		  parseMessage.patient = pt;
+		  contacts.forEach(function(elem){ parseMessage.contacts.push({name:elem}); });
 
-              contacts.forEach(function(elem){ parseMessage.contacts.push({name:elem}); });
+		  var cells = $('.contacts > tbody > .cells > td').each(function(index){
+				parseMessage.contacts[index].cell = $(this).text();
+		  });
 
-              var cells = $('.contacts > tbody > .cells > td').each(function(index){
-                parseMessage.contacts[index].cell = $(this).text();
-              });
+		  var emails = $('.contacts tbody > .emails > td').each(function(index){
+			parseMessage.contacts[index].email = $(this).text();
+		  });
 
-              var emails = $('.contacts tbody > .emails > td').each(function(index){
-                parseMessage.contacts[index].email = $(this).text();
-              });
+		  var home = $('.contacts > tbody > .phones > td').each(function(index){
+			parseMessage.contacts[index].land = $(this).text();
+		  });
 
-              var home = $('.contacts > tbody > .phones > td').each(function(index){
-                parseMessage.contacts[index].land = $(this).text();
-              });
+	          /*TODO: we must call the user's name and telecom number for the organization
+	       	   * We must declare the user variables within the portal for the EMR system
+	       	   * for an example, I'm using my cell phone number for now
+	           */
 
-              /*TODO: we must call the user's name and telecom number for the organization
-               * We must declare the user variables within the portal for the EMR system
-               * for an example, I'm using my cell phone number for now
-               */
-
-              var _get_callback = prompt("We have your number listed as (845)-371-2125: use as callback? Else, input within field");
-              if( _get_callback == '' ){
-                     console.log('beginning to call...');
-                     parseMessage.callback = "+1845-371-2125";
-                     //use user's password?
-                     console.log(parseMessage);
-                     var ciphertext = CryptoJS.AES.encrypt(JSON.stringify(parseMessage), 'portal');
-                     console.debug(ciphertext);
-              } else {
-                parseMessage.callback = _get_callback;
-                console.log(parseMessage);
-                var ciphertext = CryptoJS.AES.encrypt(parseMessage, 'portal');
-                console.debug(ciphertext);
-              };
-                /*
-                 *
-                 * AFAS COMMUNCATION SECTION 
-                 * PHASE 5-7 VALUE STREAM
-                 *
-                 */
-              afasjax(parseMessage);
-              /*
-               *
-               */
-              element.isUpdated = false;
-              element.memos = plans.length;
-              element.pres = plans;
+		  var _get_callback = prompt("We have your number listed as (845)-371-2125: use as callback? Else, input within field");
+		  if( _get_callback == '' ){
+			 console.log('beginning to call...');
+			 parseMessage.callback = "+1845-371-2125";
+			 //use user's password?
+			 console.log(parseMessage);
+			 var ciphertext = CryptoJS.AES.encrypt(JSON.stringify(parseMessage), 'portal');
+			 console.debug(ciphertext);
+		  } else {
+		    parseMessage.callback = _get_callback;
+ 		    console.log(parseMessage);
+		    var ciphertext = CryptoJS.AES.encrypt(parseMessage, 'portal');
+		    console.debug(ciphertext);
+		  };
+		  /*
+		   *
+		   * AFAS COMMUNCATION SECTION 
+		   * PHASE 5-7 VALUE STREAM
+		   *
+	           */
+		   afasjax(parseMessage);
+		  /*
+		   *
+		   */
+		   element.isUpdated = false;
+		   element.memos = plans.length;
+		   element.pres = plans;
+		};
           } else {
               element.isUpdated = false;
               element.memos = plans.length;
@@ -104,13 +109,14 @@
     window.parseForStatus = function(message,patient, prov){
       var reg = /(Condition|Medication|Procedure|Observation):?/i;
       var determine_mess = message.match(reg);
+      determine_mess = determine_mess[1].toLowerCase();
 
       if (determine_mess !== null &&
           Object.prototype.toString.call(determine_mess) == "[object Array]"){
 
-        switch (determine_mess[1]) {
+        switch (determine_mess) {
 
-          case "Condition":
+          case "condition":
             severity = determine_mess.input.match(/(severe|moderate|mild)/i);
             if (severity !== null &&
                 Object.prototype.toString.call(severity) == "[object Array]"){
@@ -130,23 +136,25 @@
             };
             break;
           
-          case "Procedure":
+          case "procedure":
             //ongoing
             console.error("Not ready for production");
             break;
 
-          case "Observation":
+          case "observation":
             //ongoing
             console.error("Not ready for production");
             break;
 
-          case "Medication":
+          case "medication":
             //ongoing 
             console.error("Not ready for production");
             break;  
         };
       } else {
         console.warn("You don't have any specific details to send to patient, please read fhi7.org for more examples ");
+        var cluster = {'practitioner': prov.name, 'callback': prov.organization.number};
+	return cluster;
       }
     };
 
